09cbed98ba0588cdf484d1e062ae4b32a83ea8ad
Rajendhar <rajendhar.mamidala@capgemini.com>
2017-05-03 10:08:39 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,6 @@
+var radius = 20; // Variable to accept radius of circle.
+var areaOfCircle = null; // Variable to Store area of circle.
+
+areaOfCircle = 3.14 * radius * radius; // Area of circle calculations
+
+console.log(areaOfCircle); // Prints Area of Circle.
